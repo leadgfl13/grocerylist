79b64a5cfcda2f9d3c58c6f19f33110162a2cb4e
leadgfl13 <leadgfl13@gmail.com>
2025-01-04 23:25:54 -0500

    Make outline of basic grocery object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import "./style.css";
 
-document.addEventListener("click", () => {
-	alert("hello");
+let testbutton = document.getElementById("test");
+testbutton.addEventListener("click", () => {
+	alert(chicken_soup.ingredients);
 });
+class Meal {
+	constructor(name, tags, ingredients) {
+		this.name = name;
+		this.tags = tags;
+		this.ingredients = ingredients;
+	}
+}
+
+let chicken_soup = new Meal(
+	"Chicken soup",
+	["dinner", "soup", "hot", "chicken"],
+	["chicken stock", "onions", "celery", "carrots", "chicken"]
+);
